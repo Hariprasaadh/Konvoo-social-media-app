124357f1521b9170fb32ee5277ba6c15e1f88113
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-07-05 15:36:00 +0530

    fetch all posts and interactions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 import CreatePost from "@/components/CreatePost"
 import { currentUser } from "@clerk/nextjs/server"
 import WhoToFollow from "@/components/WhoToFollow";
+import { getPosts } from "@/actions/post.action";
+import PostCard from "@/components/PostCard";
+import { getDBUserId } from "@/actions/user.action";
 
 const page = async () => {
 
   const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDBUserId();
+  
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>  {/*Parent 10 cols */}
 
       <div className="lg:col-span-6">   {/* 6 part for create post */ } 
           {user ? <CreatePost /> : null}
+
+          <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+
       </div>
 
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">  {/* 4 parts for follow suggestions */}
-          <WhoToFollow />
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
       </div>
 
     </div>
--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,112 +1,104 @@
-"use client"
+"use client";
 
-import { useUser } from "@clerk/nextjs"
+import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
 import { Card, CardContent } from "./ui/card";
-import { Textarea } from "./ui/textarea";
 import { Avatar, AvatarImage } from "./ui/avatar";
+import { Textarea } from "./ui/textarea";
+import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { SendIcon,Loader2Icon,ImageIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
 
-
-const CreatePost = () => {
+function CreatePost() {
 
   const { user } = useUser();
-  const [content,setContent] = useState("");
-  const [imageUrl,setImageUrl] = useState("");
-  const [isPosting,setIsPosting] = useState(false);  // to show a spinner while posting
-  const [showImageUpload, setShowImageUpload] = useState(false); // show image upload if uploading 
+  const [content, setContent] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isPosting, setIsPosting] = useState(false); // to show a spinner while posting
+  const [showImageUpload, setShowImageUpload] = useState(false);
 
   const handleSubmit = async () => {
-    if(!content.trim() && !imageUrl) return
+    // if not content or image post cant be created
+    if (!content.trim() && !imageUrl) return;
+
     setIsPosting(true);
 
-    try{
-        const result = await createPost(content, imageUrl); // server action
-        if(result.success){
-            //reset the form
-            setContent("");
-            setImageUrl("");
-            setShowImageUpload(false);
-            toast.success("Post created successfully");
-        }
-    }catch (error) {
-        console.error("Failed to create post:",error);
-        toast.error("Failed to create post");
-    }finally{
-        setIsPosting(false);
+    try {
+      const result = await createPost(content, imageUrl);
+      if (result?.success) {
+        // reset the form
+        setContent("");
+        setImageUrl("");
+        setShowImageUpload(false);
+
+        toast.success("Post created successfully");
+      }
+    } catch (error) {
+      console.error("Failed to create post:", error);
+      toast.error("Failed to create post");
+    } finally {
+      setIsPosting(false);
     }
-  }
-    
+  };
+
   return (
     <Card className="mb-6">
-        <CardContent className="pt-6">
-            <div className="space-y-4">
-                
-                {/* Show avatar and textarea to type the post content */}
-                <div className="flex space-x-4">  
+      <CardContent className="pt-6">
+        <div className="space-y-4">
+          <div className="flex space-x-4">
 
-                    <Avatar className="w-10 h-10">
-                    <AvatarImage src={user?.imageUrl || "/avatar.png"} />
-                    </Avatar>
+            <Avatar className="w-10 h-10">
+              <AvatarImage src={user?.imageUrl || "/avatar.png"} />
+            </Avatar>
 
-                    <Textarea
-                    placeholder="What's on your mind?"
-                    className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    disabled={isPosting}
-                    />
+            <Textarea
+              placeholder="What's on your mind?"
+              className="min-h-[100px] resize-none border-none focus-visible:ring-0 p-0 text-base"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              disabled={isPosting}
+            />
 
-                </div>
+          </div>
 
-                <div className="flex items-center justify-between border-t pt-4">
+          <div className="flex items-center justify-between border-t pt-4">
+            <div className="flex space-x-2">
 
-                    {/* Image Upload Button */}
-                    <div className="flex space-x-2">
-                        <Button
-                            type="button"
-                            variant="ghost"
-                            size="sm"
-                            className="text-muted-foreground hover:text-primary"
-                            onClick={() => setShowImageUpload(!showImageUpload)}
-                            disabled={isPosting}
-                        >
-                        <ImageIcon className="size-4 mr-2" />
-                        Photo
-                        </Button>
-                    </div>
-
-                    {/* Post Button */}
-                    {/* Button disabled if no post or image content */}
-                    <Button
-                        className="flex items-center"
-                        onClick={handleSubmit} 
-                        disabled={(!content.trim() && !imageUrl) || isPosting}
-                    >
-                    {isPosting ? (
-                        <>
-                        <Loader2Icon className="size-4 mr-2 animate-spin" />
-                        Posting...
-                        </>
-                    ) : (
-                        <>
-                        <SendIcon className="size-4 mr-2" />
-                        Post
-                        </>
-                    )}
-                    </Button>
-
-                </div>
-
+              <Button
+                type="button"
+                variant="ghost"
+                size="sm"
+                className="text-muted-foreground hover:text-primary"
+                onClick={() => setShowImageUpload(!showImageUpload)}
+                disabled={isPosting}
+              >
+                <ImageIcon className="size-4 mr-2" />
+                Photo
+              </Button>
             </div>
-
-        </CardContent>
-
+            
+            <Button
+              className="flex items-center"
+              onClick={handleSubmit}
+              disabled={(!content.trim() && !imageUrl) || isPosting}
+            >
+              {isPosting ? (
+                <>
+                  <Loader2Icon className="size-4 mr-2 animate-spin" />
+                  Posting...
+                </>
+              ) : (
+                <>
+                  <SendIcon className="size-4 mr-2" />
+                  Post
+                </>
+              )}
+            </Button>
+          </div>
+        </div>
+      </CardContent>
     </Card>
-  )
+  );
 }
-
-export default CreatePost
+export default CreatePost;
